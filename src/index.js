a2e6dfd5cbebe0a61b86b3535a80699b1ba64661
manlio <manliot@uninorte.edu.co>
2020-04-20 21:32:42 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
-const app = require('./app');
-require('./database')
 
-async function main() {
-    await app.listen(app.get('port'));
-    console.log('server on port ',app.get('port'));
+
+const app=require('./app');
+require('./database');
+async function main(){
+  app.set('puerto', process.env.PORT || 4000);
+  await app.listen(app.get('puerto'));
+  console.log('Example app listening on port'+ app.get('puerto'));
 }
 
-main()
 
+main();
